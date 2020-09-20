f8546465be2dfb740be422ea5a0cfbc476d235a6
Leonard Poissant <leonardtati@gmail.com>
2020-09-20 10:35:56 -0400

    added proxy to client

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "../src/Components/App";
+import App from "./Components/App";
 
 import HaikuDataBaseProvider from "./HaikuContext/HaikuDataBaseContext";
 import HaikuGeneratorProvider from "./HaikuContext/HaikuGeneratorContext";
